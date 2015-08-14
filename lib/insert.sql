d85e9bd16138ec0f47dc96317a5133486e2b3e2b
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-14 14:21:45 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Homer", 20, "M", "brown", "lazy", "TRUE"),
+(2,"Bart", 17, "F", "blue", "intense", "TRUE"),
+(3,"Lisa", 5, "F", "brown", "Sweet", "TRUE"),
+(4,"Marge", 16, "M", "Green", "Sick", "FALSE"),
+(5,"Maggie", 15, "M", "orange", "grim", "FALSE"),
+(6,"Santa's Little Helper", 4, "M", "Black", "Grinchy", "TRUE"),
+(7,"Ned Flanders", 23, "F", "Blue", "naive", "TRUE"),
+(8,null, 21, "M", "black", "aggressive", "TRUE");
